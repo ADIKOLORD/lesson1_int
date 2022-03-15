5dfd91e2a79929a9b162d99337db4f3026c4e4e2
ADIKOLORD <adiko23082004@gmail.com>
2022-03-15 10:09:35 +0600

    lesson about integer

---
+++ lesson1.py
@@ -0,0 +1,101 @@
+'''
+print('Problem 4')
+print((17 * 3) > (12 * 5))
+print((12 ** 3) > (13 * 7))
+print((4 ** 5) > (512 + 512))
+
+print('Problem 1')
+print(17925 > (34 ** 2))
+print(17925 > (26 * 3))
+print(17925 > (17 * 33))
+print(17925 > (4394 * 4))
+
+print('Problem 3')
+print(45 + 55)
+print((45 + 55) * 5)
+print(((45 + 55) * 5) ** 3)
+print((((45 + 55) * 5) ** 3) - 193432)
+
+print('Problem 2')
+a = 5
+b = 3
+c = 7
+print(c > a > b)
+print((7 % 3) * 4.8)
+print((45 + 36) == 9 ** 2)
+print((3 ** 3) != (5 * 5))
+
+print('Problem 5')
+print(-21 // 10)
+
+print('Problem 9')
+birth_day = 2004
+year = 2022
+print((year - birth_day) - 2)
+print((year - birth_day) + 2)
+
+print('Problem 8')
+a = 18
+b = 100
+print((a / b) * 100)
+
+print('Problem 10')
+print((25 + 75 + 10 + 95) / 4)
+
+print('Problem 17')
+one, two, three = 5, 5, 5
+print((one + two) * three)
+
+print('Problem 14')
+a = 10
+b = 11
+c = 18.9
+d = 7
+e = 99
+
+print('Problem 13')
+print((a-e ** (b/d)) % c)
+
+'''
+
+
+
+'''
+# Problem 0
+name = input("Your full name: ").split()
+print(name[0].lower(), name[1].upper())
+
+# Problem 1
+print(str(2 > 1))
+
+# Problem 2
+chair = input('Enter one chair: ')
+print('GitHub'.split(chair))
+
+# Problem 3
+word = 'Ботнет IPStorm , в который ранее входили лишь Windows-машины, увеличился до 13 500 зараженных систем'
+print(word.replace('е', '3'))
+
+# Problem 4
+name_age_film = input('Text your name, age, favorite movie: ').split()
+print(f'Hello {name_age_film[0]} ,\nthe movie {name_age_film[2]} is very good!')
+
+# Problem 5
+word = 'Google создаст специальную команду для поиска багов в особо важных приложениях'.split()
+print(len(word))
+'''
+
+# Problem 6
+word = 'Самые важные собЫтия в миРе инфосека за сентябрь'
+print(word.replace('', '|'))
+
+# Problem 7
+word = 'хакеры слили в сеть данные пакистанской энергетической компании k-Electric'.title()
+print(word)
+
+a = int(input())
+print(a)
+print(type(a))
+
+
+
