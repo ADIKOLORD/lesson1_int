7d7591fbdc7274b8a71b7f5c7033f0aaf4f1496e
Kyrgyzbaev ADILET <98408321+ADIKOLORD@users.noreply.github.com>
2022-07-23 23:59:04 +0600

    Update lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -2,7 +2,7 @@ print('Problem 4')
 print((17 * 3) > (12 * 5))
 print((12 ** 3) > (13 * 7))
 print((4 ** 5) > (512 + 512))
-sdddddssaaqsssssssdddddsbm
+sdddddssaaqsssssssdddddsbmn
 print('Problem 1')
 print(17925 > (34 ** 2))
 print(17925 > (26 * 3))
